33eca367439d98f879d0ecd341a027e0a2d2bfc6
InakiGT <yarimfuego@gmail.com>
2025-07-09 20:35:02 -0600

    [ADDED] Sintaxis para validar servidor en swagger

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config()
 
 const config = {
   port: process.env.PORT || 3000,
-	server: process.env.SERVER || 'http://localhost',
+	server: process.env.SERVER || 'http://localhost:' + process.env.PORT,
 	mongoURI: process.env.MONGO_URI || '',
 	jwtSecret: process.env.JWT_SECRET || '',
 }
